5882c65f3e01bba32ecec77e867e04b168ff31b3
Bob <Bob@LAPTOP>
2017-10-31 22:55:10 +0100

    added
    page vmm
    newcriteria vmm

---
+++ Matricis/Matricis/Views/NewCriteriaPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Matricis.Models;
+using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Matricis.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewCriteriaPage : ContentPage {
+
+        public Criteria Criteria { get; set; }
+
+        public NewCriteriaPage() {
+            InitializeComponent();
+
+            Criteria = new Criteria {
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e) {
+            MessagingCenter.Send(this, "AddCriteria", Criteria);
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
