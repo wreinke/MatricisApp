4d8a2cfe8dc6d0a39aff0d8b80382e18c57d89b5
Bob <Bob@LAPTOP>
2017-11-07 22:14:30 +0100

    tablepage eingefügt

---
+++ Matricis/Matricis/ViewModels/TableViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Matricis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Matricis.ViewModels {
+    public class TableViewModel : BaseViewModel {
+        private Evaluation currentEvaluation;
+
+        public Evaluation CurrentEvaluation
+        {
+            get
+            {
+                return currentEvaluation;
+            }
+
+            set
+            {
+                SetProperty(ref currentEvaluation, value);
+            }
+        }
+
+        public TableViewModel() {
+            MessagingCenter.Subscribe<EvaluationsViewModel, Evaluation>(this, "EvaluationSelectedM", (sender, args) => {
+                CurrentEvaluation = args;
+            });
+        }
+
+
+
+    }
+}
